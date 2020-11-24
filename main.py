ebb4f7b5dcc7623e21636f1e4ccb6a01ec786fad
korlamarch <khemarat_boonyapaluk@brown.edu>
2020-11-24 00:11:22 -0500

    add test/train

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ import numpy as np
 
 from model import Model
 
+def train(model, data):
+    pass
+
+def test(model, data):
+    pass
+
 def main():
     model = Model()
+    train(model, None)
+    test(model, None)
 
 if __name__ == '__main__':
     main()
