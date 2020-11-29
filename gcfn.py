a50be6c0b1a24b7eea1d3b518e7909d2c0971be3
Kota Soda <kota_soda@brown.edu>
2020-11-29 11:44:25 +0900

    adding in more functions

--- gcfn.py
+++ gcfn.py
@@ -11,7 +11,7 @@ from tensorflow.keras import Model
 """
 class GatedFusionModule(tf.keras.Model):
     def __init__(self, corr, f_s_1, f_s_2, f_s_3, f_rh_1, f_rh_2, f_rh_3):
-        super(Model, self).__init__()
+        super(GatedFusionModule, self).__init__()
         """ Init input matrices """
         self.corr = corr
         self.f_s_1 = f_s_1
@@ -38,7 +38,7 @@ class GatedFusionModule(tf.keras.Model):
 """
 class SemanticAssignmentModule(tf.keras.Model):
     def __init__(self, t_lum, r_lum, r_ab):
-        super(Model, self).__init__()
+        super(SemanticAssignmentModule, self).__init__()
         """ Init input matrices """
         self.t_lum = t_lum
         self.r_lum = r_lum
@@ -55,14 +55,23 @@ class SemanticAssignmentModule(tf.keras.Model):
     Color Distribution Module
     Input: histogram of reference image
     Output: 3 features matrices of R_h of different sizes
+            *these matrices are pre-spacial replication
 """
 class ColorDistributionModule(tf.keras.Model):
     def __init__(self, r_hist):
-        super(Model, self).__init__()
+        super(ColorDistributionModule, self).__init__()
         """ Init input matrices """
         self.r_hist = r_hist
+        self.in_dims = tf.get_shape(r_hist)[3]
 
         """ Init layers """
+        self.conv_1_1 = tf.keras.layers.Conv2D(512, 1, activation='relu', padding='same')
+        self.conv_1_2 = tf.keras.layers.Conv2D(512, 1, activation='relu', padding='same')
+        self.conv_1_3 = tf.keras.layers.Conv2D(512, 1, activation='relu', padding='same')
+
+        self.conv_2_1 = tf.keras.layers.Conv2D(512, 1, activation='relu', padding='same')
+        self.conv_2_2 = tf.keras.layers.Conv2D(256, 1, activation='relu', padding='same')
+        self.conv_2_3 = tf.keras.layers.Conv2D(128, 1, activation='relu', padding='same')
 
     def call(self, inputs, is_testing=False):
         pass
--- mcn.py
+++ mcn.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras import Model
 
+""" Make Resblock Class hers """
+
 """ 
     MCN Encoder 
     Input: luminance channel of input image and reference image
@@ -10,12 +12,35 @@ from tensorflow.keras import Model
 """
 class Encoder(tf.keras.Model):
     def __init__(self, r_l, t_l, m_1, m_2, m_3):
-        super(Model, self).__init__()
+        super(Encoder, self).__init__()
         """ Init input matrices """
         self.r_l = r_l
         self.t_l = t_l
 
         """ Init layers """
+        self.conv_1_1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')
+        self.conv_1_2 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')
+        self.batch_norm_1 = tf.keras.layers.BatchNormalization()
+
+        self.conv_2_1 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')
+        self.conv_2_2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')
+        self.batch_norm_2 = tf.keras.layers.BatchNormalization()
+
+        self.conv_3_1 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')
+        self.conv_3_2 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')
+        self.conv_3_3 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')
+        self.batch_norm_3 = tf.keras.layers.BatchNormalization()
+
+        self.conv_4_1 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')
+        self.conv_4_2 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')
+        self.conv_4_3 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')
+        self.batch_norm_4 = tf.keras.layers.BatchNormalization()
+
+        """ add dilation 1 """
+
+        """ add dilation 2 """
+        
+
 
     def call(self, inputs, is_testing=False):
         pass
@@ -29,12 +54,16 @@ class Encoder(tf.keras.Model):
 """
 class Decoder1(tf.keras.Model):
     def __init__(self, t_l, m_1):
-        super(Model, self).__init__()
+        super(Decoder1, self).__init__()
         """ Init input matrices """
         self.t_l = t_l
         self.m_1 = m_1
 
         """ Init layers """
+        self.resconv_1 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')
+        self.batch_norm_1 = tf.keras.layers.BatchNormalization()
+        """ add 2 resblocks """
+        """ conv2dtranspose here """
 
     def call(self, inputs, is_testing=False):
         pass
@@ -48,12 +77,16 @@ class Decoder1(tf.keras.Model):
 """
 class Decoder2(tf.keras.Model):
     def __init__(self, t_l, m_2):
-        super(Model, self).__init__()
+        super(Decoder2, self).__init__()
         """ Init input matrices """
         self.t_l = t_l
         self.m_2 = m_2
 
         """ Init layers """
+        self.resconv_1 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')
+        self.batch_norm_1 = tf.keras.layers.BatchNormalization()
+        """ add 2 resblocks """
+        """ conv2dtranspose here """
 
     def call(self, inputs, is_testing=False):
         pass
@@ -67,12 +100,16 @@ class Decoder2(tf.keras.Model):
 """
 class Decoder3(tf.keras.Model):
     def __init__(self, t_l, m_3):
-        super(Model, self).__init__()
+        super(Decoder3, self).__init__()
         """ Init input matrices """
         self.t_l = t_l
         self.m_3 = m_3
 
         """ Init layers """
+        self.resconv_1 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')
+        self.batch_norm_1 = tf.keras.layers.BatchNormalization()
+        """ add 2 resblocks """
+        """ conv2dtranspose here """
 
     def call(self, inputs, is_testing=False):
         pass
