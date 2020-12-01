12860e1b76d777b88b5ba3bb90304228b8a46707
Khemarat Boonyapaluk <korla.march@gmail.com>
2020-12-01 01:16:40 -0500

    model implementation first draft

--- gcfn.py
+++ gcfn.py
@@ -9,6 +9,8 @@ from tensorflow.keras import Model
            6 feature matrices of 3 different sizes
     Output: 3 fused color features of 3 different sizes
 """
+
+
 class GatedFusionModule(tf.keras.Model):
     def __init__(self):
         super(GatedFusionModule, self).__init__()
@@ -16,13 +18,23 @@ class GatedFusionModule(tf.keras.Model):
         """ Init layers """
         self.kernel_size = 3
 
-        self.C_conv_1 = tf.keras.layers.Conv2D(64, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
-        self.C_conv_2 = tf.keras.layers.Conv2D(128, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
+        self.C_conv_1 = tf.keras.layers.Conv2D(
+            64, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
+        self.C_conv_2 = tf.keras.layers.Conv2D(
+            128, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
 
-    def call(self, corr, f_s_1, f_s_2, f_s_3, f_rh_1, f_rh_2, f_rh_3, is_testing=False):
-        corr = self.C_conv_1(corr)
-        corr = self.C_conv_2(corr)
+    def call(self, corr, align_1, align_2, align_3, global_1, global_2, global_3, is_testing=False):
+        conf = self.C_conv_1(corr)
+        conf = self.C_conv_2(corr)
 
+        conf_1 = conf
+        conf_2 = conf_1[:, :, ::2, ::2]
+        conf_3 = conf_2[:, :, ::2, ::2]
+
+        M3 = align_3 * conf_3 + global_1 * (1 - conf_3)
+        M2 = align_2 * conf_2 + global_2 * (1 - conf_2)
+        M1 = align_1 * conf_1 + global_3 * (1 - conf_1)
+        
         return M1, M2, M3
 
 
@@ -35,31 +47,38 @@ class GatedFusionModule(tf.keras.Model):
             correlation matrix C,
             3 color feature matrices of different sizes
 """
+
+
 class SemanticAssignmentModule(tf.keras.Model):
     def __init__(self, num_classes):
         super(SemanticAssignmentModule, self).__init__()
-        
+
         """ self.height = height """
         """ self.width = width """
 
         self.kernel_size = 3
         self.num_classes = num_classes
 
-        self.rab_conv_1 = tf.keras.layers.Conv2D(64, self.kernel_size, activation='relu', padding='same')
-        self.rab_conv_2 = tf.keras.layers.Conv2D(128, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
+        self.rab_conv_1 = tf.keras.layers.Conv2D(
+            64, self.kernel_size, activation='relu', padding='same')
+        self.rab_conv_2 = tf.keras.layers.Conv2D(
+            128, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
 
-        self.fa_conv_1 = tf.keras.layers.Conv2D(128, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
-        self.fa_conv_2 = tf.keras.layers.Conv2D(256, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
-        self.fa_conv_3 = tf.keras.layers.Conv2D(512, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
+        self.fa_conv_1 = tf.keras.layers.Conv2D(
+            128, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
+        self.fa_conv_2 = tf.keras.layers.Conv2D(
+            256, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
+        self.fa_conv_3 = tf.keras.layers.Conv2D(
+            512, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
 
         self.gtl_dense_1 = tf.keras.layers.Dense(512, activation='relu')
-        self.gtl_dense_2 = tf.keras.layers.Dense(num_classes, activation='softmax')
-
+        self.gtl_dense_2 = tf.keras.layers.Dense(
+            num_classes, activation='softmax')
 
     def call(self, t_lum, r_lum, r_ab, is_testing=False):
         """ takes in r, t, output of encoder, r_ab and spit out correlation matrix features conf_1,2,3, class output G, and f_s1,2,3 """
         C = self.correlate(t_lum, r_lum)
-        
+
         f_rab = self.rab_conv_1(r_ab)
         f_rab = self.rab_conv_2(f_rab)
 
@@ -68,11 +87,11 @@ class SemanticAssignmentModule(tf.keras.Model):
         f_s2 = self.fa_conv_2(f_s3)
         f_s1 = self.fa_conv_3(f_s2)
 
-        mp = tf.nn.max_pool2d(t_lum[3], t_lum.shape, strides=1, padding="VALID")
+        mp = tf.nn.max_pool2d(t_lum[3], t_lum.shape,
+                              strides=1, padding="VALID")
 
         g_tl = self.gtl_dense_1(tf.reshape(mp, [len(mp), -1]))
         g_tl = self.gtl_dense_2(g_tl)
-        g_tl = self.gtl_dense_3(g_tl)
 
         return (g_tl, C, f_s1, f_s2, f_s3)
 
@@ -82,7 +101,7 @@ class SemanticAssignmentModule(tf.keras.Model):
         for i in range(len(t_lum)):
             for j in range(len(r_lum)):
                 norm_fac = tf.norm(t_lum[i]) * tf.norm(r_lum[j])
-                dot_prod = tf.tensordot(t_lum[i], r_lum[j], axis=0)
+                dot_prod = tf.tensordot(t_lum[i], r_lum[j], axes=0)
                 C[i, j] = norm_fac / dot_prod
         return C
 
@@ -90,11 +109,11 @@ class SemanticAssignmentModule(tf.keras.Model):
         # TODO: optimize this with tensordot
         exp_c = tf.exp(C)
         aij = exp_c / tf.reduce_sum(exp_c, axis=1)
-        
+
         f_a = tf.zeros_like(f_rab)
         for i in range(len(aij)):
-            f_a[i] = tf.sum(aij[i] * f_rab, axis=0)
-        
+            f_a[i] = tf.reduce_sum(aij[i] * f_rab, axis=0)
+
         return f_a
 
 
@@ -104,6 +123,8 @@ class SemanticAssignmentModule(tf.keras.Model):
     Output: 3 features matrices of R_h of different sizes
             *these matrices are pre-spacial replication
 """
+
+
 class ColorDistributionModule(tf.keras.Model):
     def __init__(self):
         super(ColorDistributionModule, self).__init__()
@@ -111,13 +132,19 @@ class ColorDistributionModule(tf.keras.Model):
         self.kernel_size = 1
 
         """ Init layers """
-        self.conv_1_1 = tf.keras.layers.Conv2D(512, self.kernel_size, activation='relu', padding='same')
-        self.conv_1_2 = tf.keras.layers.Conv2D(512, self.kernel_size, activation='relu', padding='same')
-        self.conv_1_3 = tf.keras.layers.Conv2D(512, self.kernel_size, activation='relu', padding='same')
+        self.conv_1_1 = tf.keras.layers.Conv2D(
+            512, self.kernel_size, activation='relu', padding='same')
+        self.conv_1_2 = tf.keras.layers.Conv2D(
+            512, self.kernel_size, activation='relu', padding='same')
+        self.conv_1_3 = tf.keras.layers.Conv2D(
+            512, self.kernel_size, activation='relu', padding='same')
 
-        self.conv_2_1 = tf.keras.layers.Conv2D(512, self.kernel_size, activation='relu', padding='same')
-        self.conv_2_2 = tf.keras.layers.Conv2D(256, self.kernel_size, activation='relu', padding='same')
-        self.conv_2_3 = tf.keras.layers.Conv2D(128, self.kernel_size, activation='relu', padding='same')
+        self.conv_2_1 = tf.keras.layers.Conv2D(
+            512, self.kernel_size, activation='relu', padding='same')
+        self.conv_2_2 = tf.keras.layers.Conv2D(
+            256, self.kernel_size, activation='relu', padding='same')
+        self.conv_2_3 = tf.keras.layers.Conv2D(
+            128, self.kernel_size, activation='relu', padding='same')
 
     def call(self, r_hist, is_testing=False):
         conv_output = self.conv_1_3(self.conv_1_2(self.conv_1_1(r_hist)))
--- model.py
+++ model.py
@@ -3,10 +3,11 @@ import numpy as np
 from gcfn import GatedFusionModule, SemanticAssignmentModule, ColorDistributionModule
 from mcn import Encoder, Decoder1, Decoder2, Decoder3
 
+
 class Model(tf.keras.Model):
     def __init__(self):
         super(Model, self).__init__()
-        
+
         self.encoder = Encoder()
         self.decoder_1 = Decoder1()
         self.decoder_2 = Decoder2()
@@ -17,16 +18,21 @@ class Model(tf.keras.Model):
 
     def call(self, r_hist, r_ab, r_l, t_l, is_testing=False):
         """ get features and output of convolution layers from encoder """
-        f_rl, f_tl, conv_out = self.encoder(r_l, t_l)
-        
+        f_rl, f_tl, enc_output_1, layer_1, layer_2, layer_3 = self.encoder(
+            r_l, t_l)
+
         f_global1, f_global2, f_global3 = self.cdm(r_hist)
-        g_tl, corr, f_s1, f_s2, f_s3 = self.sam(f_twannal, f_rl, r_ab)
-        
-        m1, m2, m3 = self.gfm(corr, f_s1, f_s2, f_s3, f_global1, f_global2, f_global3)
-        
-        encoder_output, fake_img_1 = self.Decoder1(distri, assign, corr_feat_3, enc_output_1, layer_3)
-        encoder_output, fake_img_2 = self.Decoder2(distri, assign, corr_feat_2, enc_output_2, layer_2)
-        encoder_output, fake_img_3 = self.Decoder3(distri, assign, corr_feat_1, enc_output_1, layer_1)
+        g_tl, corr, f_s1, f_s2, f_s3 = self.sam(f_tl, f_rl, r_ab)
+
+        gate_out1, gate_out2, gate_out3 = self.gfm(
+            corr, f_s1, f_s2, f_s3, f_global1, f_global2, f_global3)
+
+        decoder_output1, fake_img_1 = self.decoder_1(
+            gate_out1, enc_output_1, layer_3)
+        decoder_output2, fake_img_2 = self.decoder_2(
+            gate_out2, decoder_output1, layer_2)
+        decoder_output3, fake_img_3 = self.decoder_3(
+            gate_out3, decoder_output2, layer_1)
 
         return g_tl, fake_img_1, fake_img_2, fake_img_3
 
@@ -36,7 +42,8 @@ class Model(tf.keras.Model):
                3 ab channel matrices of input image T
                luminance channel of input image T
         Output: Loss
-    """ 
+    """
+
     def loss(self, g, t_ab_1, t_ab_2, t_ab_3, t_l):
 
         def loss_class(g_tl):
@@ -53,5 +60,5 @@ class Model(tf.keras.Model):
 
         def loss_G(t_ab, t_l):
             pass
-        
+
         pass
