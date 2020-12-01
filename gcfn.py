094ccdad3480096c03ec88d33bc5747f5c6c41c6
Khemarat Boonyapaluk <korla.march@gmail.com>
2020-11-30 21:54:30 -0500

    add SAM

--- gcfn.py
+++ gcfn.py
@@ -14,9 +14,14 @@ class GatedFusionModule(tf.keras.Model):
         super(GatedFusionModule, self).__init__()
 
         """ Init layers """
+        self.kernel_size = 3
+
+        self.C_conv_1 = tf.keras.layers.Conv2D(64, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
+        self.C_conv_2 = tf.keras.layers.Conv2D(128, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
 
     def call(self, corr, f_s_1, f_s_2, f_s_3, f_rh_1, f_rh_2, f_rh_3, is_testing=False):
-        pass
+        corr = self.C_conv_1(corr)
+        corr = self.C_conv_2(corr)
 
 
 """ 
@@ -29,14 +34,15 @@ class GatedFusionModule(tf.keras.Model):
             3 color feature matrices of different sizes
 """
 class SemanticAssignmentModule(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, num_classes):
         super(SemanticAssignmentModule, self).__init__()
         
         """ self.height = height """
         """ self.width = width """
 
         self.kernel_size = 3
-        
+        self.num_classes = num_classes
+
         self.rab_conv_1 = tf.keras.layers.Conv2D(64, self.kernel_size, activation='relu', padding='same')
         self.rab_conv_2 = tf.keras.layers.Conv2D(128, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
 
@@ -44,10 +50,52 @@ class SemanticAssignmentModule(tf.keras.Model):
         self.fa_conv_2 = tf.keras.layers.Conv2D(256, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
         self.fa_conv_3 = tf.keras.layers.Conv2D(512, self.kernel_size, strides=(2, 2), activation='relu', padding='same')
 
+        self.gtl_dense_1 = tf.keras.layers.Dense(1024, activation='relu')
+        self.gtl_dense_2 = tf.keras.layers.Dense(1024, activation='relu')
+        self.gtl_dense_3 = tf.keras.layers.Dense(num_classes, activation='softmax')
+
 
     def call(self, t_lum, r_lum, r_ab, is_testing=False):
         """ takes in r, t, output of encoder, r_ab and spit out correlation matrix features conf_1,2,3, class output G, and f_s1,2,3 """
-        pass
+        C = self.correlate(t_lum, r_lum)
+        
+        f_rab = self.rab_conv_1(r_ab)
+        f_rab = self.rab_conv_2(f_rab)
+
+        f_a = self.attention(C, f_rab)
+        f_s3 = self.fa_conv_1(f_a)
+        f_s2 = self.fa_conv_2(f_s3)
+        f_s1 = self.fa_conv_3(f_s2)
+
+        mp = tf.nn.max_pool(t_lum[3], 2, strides=2, padding='same')
+
+        g_tl = self.gtl_dense_1(tf.reshape(mp, [len(mp), -1]))
+        g_tl = self.gtl_dense_2(g_tl)
+        g_tl = self.gtl_dense_3(g_tl)
+
+        return (g_tl, C, f_s3, f_s2, f_s1)
+
+    def correlate(self, t_lum, r_lum):
+        # TODO: optimize this
+        C = tf.zeros(len(t_lum), len(r_lum))
+        for i in range(len(t_lum)):
+            for j in range(len(r_lum)):
+                norm_fac = tf.norm(t_lum[i]) * tf.norm(r_lum[j])
+                dot_prod = tf.tensordot(t_lum[i], r_lum[j], axis=0)
+                C[i, j] = norm_fac / dot_prod
+        return C
+
+    def attention(self, C, f_rab):
+        # TODO: optimize this with tensordot
+        exp_c = tf.exp(C)
+        aij = exp_c / tf.reduce_sum(exp_c, axis=1)
+        
+        f_a = tf.zeros_like(f_rab)
+        for i in range(len(aij)):
+            f_a[i] = tf.sum(aij[i] * f_rab, axis=0)
+        
+        return f_a
+
 
 """ 
     Color Distribution Module
